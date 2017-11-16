78bb72878f9b953dead56fd9a5868ca6ed756f8e
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2017-11-16 10:28:57 -0500

    78.5% accuracy

--- titanic/logistic_regression.R
+++ titanic/logistic_regression.R
@@ -13,7 +13,7 @@ prepare = function (data.orig) {
   encoded = model.matrix(~ (Sex-1) + (Embarked-1), data=data, 
                          contrasts.arg=list(Sex=contrasts(data$Sex, contrasts=F),
                                             Embarked=contrasts(data$Embarked, contrasts=F)))
-  data = cbind(data.orig[,c("Pclass", "Age", "Fare")], encoded)
+  data = cbind(data.orig[,c("Pclass", "Age")], encoded)
   
   # fill up null entries
   data[, "Age"][is.na(data[, "Age"])] <- mean(data[,"Age"], na.rm=TRUE)
@@ -37,18 +37,18 @@ mu = mean(train.data[, "Age"], na.rm=TRUE)
 train.data[, "Age"] = train.data[, "Age"] - mu
 norm_param[1, 'offset'] = mu
 
-mu = mean(train.data[, "Fare"], na.rm=TRUE)
-train.data[, "Fare"] = train.data[, "Fare"] - mu
-norm_param[2, 'offset'] = mu
+#mu = mean(train.data[, "Fare"], na.rm=TRUE)
+#train.data[, "Fare"] = train.data[, "Fare"] - mu
+#norm_param[2, 'offset'] = mu
 
 # scale down by standard deviation
 std = sd(train.data[, "Age"])
 train.data[, "Age"] = train.data[, "Age"] / std
 norm_param[1, 'scale'] = std
 
-std = sd(train.data[, "Fare"])
-train.data[, "Fare"] = train.data[, "Fare"] / std
-norm_param[2, 'scale'] = std
+#std = sd(train.data[, "Fare"])
+#train.data[, "Fare"] = train.data[, "Fare"] / std
+#norm_param[2, 'scale'] = std
 
 # preprocess testing set
 test.data.orig = read.csv("test.csv")
@@ -57,16 +57,16 @@ test.data = as.matrix(sapply(test.data, as.numeric))
 
 # normalize and standardize data
 test.data[, "Age"] = (test.data[, "Age"] - norm_param[1, 'offset']) / norm_param[1, 'scale']
-test.data[, "Fare"] = (test.data[, "Fare"] - norm_param[2, 'offset']) / norm_param[2, 'scale']
+# test.data[, "Fare"] = (test.data[, "Fare"] - norm_param[2, 'offset']) / norm_param[2, 'scale']
 
 #-----------------------------------------------------------#
 
-## Linear Logistic Regression ##
+## Logistic Ridge Regression ##
 
-cvfit<-cv.glmnet(train.data,response,family="binomial",type.measure="class", alpha=0)
+cvfit = cv.glmnet(train.data,response,family="binomial",type.measure="class", alpha=0)
 lambda = cvfit$lambda.1se
 lambda
-# lambda:  0.1512091
+# lambda:  0.03833703
 
 # perform regularized logistic regression
 fit = glmnet(train.data, response, family="binomial", lambda=lambda, alpha=0)
@@ -77,8 +77,10 @@ beta = fit$beta
 
 # normalize and allocate predictions
 Survived = predict(fit, test.data, type="class")
+Survived[is.na(Survived)] = 0
 PassengerId = c((dim(train.data)[1]+1):(length(Survived)+dim(train.data)[1]))
 df = data.frame(PassengerId, Survived)
+colnames(df)[2] = "Survived"
 write.csv(df, file = "predictions.csv", row.names=FALSE)
 
 #-----------------------------------------------------------#
